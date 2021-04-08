a59dc2b618756e0725c74a0aed47a06d9a858505
Lauretta S.P. Cheng <53014890+lspcheng@users.noreply.github.com>
2021-04-08 11:55:31 -0400

    Add vowel and stress flags

--- youspeak/get-vowels.py
+++ youspeak/get-vowels.py
@@ -167,7 +167,16 @@ def main(args):
 
                         # Get label word/sound info
                         int_vowel = int_lab[:2]
+                        if args.vowels:
+                            vowel_list = [v for v in args.vowels.split(',')]
+                            if int_vowel not in vowel_list:
+                                continue
                         int_stress = int_lab[-1]
+                        if args.stress:
+                            stress_list = [s for s in args.stress.split(',')]
+                            if int_stress not in int_stress:
+                                continue
+
                         int_word = call(textgrid, 'Get label of interval', 1,
                                     call(textgrid, 'Get interval at time', 1, int_start))
                         # print(int_word)
@@ -226,6 +235,8 @@ if __name__ == '__main__':
     parser.add_argument('--channel', '-ch', default=None, type=str, help='channel folder')
     parser.add_argument('--video', '-v', default=None, type=str, help='video number')
     parser.add_argument('--formants', '-f', default=3, type=int, help='maximum number of formants to extract (default=3)')
+    parser.add_argument('--vowels', '-vw', help='list of vowels to target, comma-separated', type=str)
+    parser.add_argument('--stress', '-st', help='list of stress values to target, comma-separated', type=str)
     parser.add_argument('--nucleus', '-n', action='store_true', default=False, help='extract nucleus midpoint formants')
     parser.add_argument('--onoff', '-o', action='store_true', default=False, help='extract onset and offset formants')
     parser.add_argument('--steps', '-s', action='store_true', default=False, help='extract formants at 30 steps')

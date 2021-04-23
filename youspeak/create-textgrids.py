224007fa3853e6284d45f3786be80e3146898c6e
Lauretta S.P. Cheng <53014890+lspcheng@users.noreply.github.com>
2021-04-23 15:09:32 -0400

    Refactor and standardize naming

--- youspeak/create-textgrids.py
+++ youspeak/create-textgrids.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
-import os
 from os import listdir, makedirs, path
-import shutil
 from glob import glob
 import pandas as pd
 
@@ -25,43 +23,38 @@ def main(args):
     if args.channel:
         channel_list = [args.channel]
     else:
-        channel_list = [channel for channel in listdir(path.join(chunked_audio_base, "logs", "coding")) if not channel.startswith('.')]
+        channel_list = [channel_id for channel_id in listdir(path.join(chunked_audio_base, "logs", "coding")) if not channel_id.startswith('.')]
 
-    for channel in channel_list:
-        logpath = path.join(chunked_audio_base, "logs", "coding", channel)
+    for channel_id in channel_list:
+        log_path = path.join(chunked_audio_base, "logs", "coding", channel_id)
 
-        if args.video:
-            video_id = '{0}_{1}'.format(channel, args.video)
-            video_list = [video_id]
-            # print(video_list)
-        else:
-            video_list = [fname.split('_coding')[0] for fname in listdir(logpath) if not fname.startswith('.')]
-            # print(video_list)
+        video_list = [fn.split('_coding')[0] for fn in listdir(log_path) if not fn.startswith('.')]
 
         for video_id in video_list:
-            audpath = path.join(chunked_audio_base, "audio", "chunking", channel, video_id)
-            fname = path.join(logpath, video_id+'_coding_responses.csv')
-            if path.isdir(out_audpath) and not args.overwrite:
-                existing_files = glob(path.join(out_audpath, "**", "*{0}*".format(video_id)), recursive=True)
+            # output dirs
+            out_audio_path = path.join(chunked_audio_base, "audio", "coding", channel_id, video_id)
+            out_tg_path = path.join(chunked_audio_base, "textgrids", "coding", channel_id, video_id)
+
+            if path.isdir(out_audio_path) and not args.overwrite:
+                existing_files = glob(path.join(out_audio_path, "**", "*{0}*".format(video_id)), recursive=True)
                 if existing_files:
                     continue
 
-            df = pd.read_csv(fname)
+            audio_path = path.join(chunked_audio_base, "audio", "chunking", channel_id, video_id)
+            file_path = path.join(log_path, video_id+'_coding_responses.csv')
+
+            df = pd.read_csv(file_path)
             print('Processing audio chunks from: {0}'.format(video_id))
 
-            # output dirs
-            out_audpath = path.join(chunked_audio_base, "audio", "coding", channel, video_id)
-            out_tgpath = path.join(chunked_audio_base, "textgrids", "coding", channel, video_id)
-
             # alignment dirs
-            prealignpath = path.join(aligned_audio_base, "original_corpus", channel, video_id)
-            postalignpath = path.join(aligned_audio_base, "aligned_corpus", channel, video_id)
-            alignerpath = path.join(aligned_audio_base, "mfa_aligner")
-            adjusted_queuepath = path.join(aligned_audio_base, "adjusted_corpus", channel, video_id, "queue")
-            adjusted_audpath = path.join(aligned_audio_base, "adjusted_corpus", channel, video_id, "audio")
-            adjusted_tgpath = path.join(aligned_audio_base, "adjusted_corpus", channel, video_id, "textgrids")
+            pre_align_path = path.join(aligned_audio_base, "original_corpus", channel_id, video_id)
+            post_align_path = path.join(aligned_audio_base, "aligned_corpus", channel_id, video_id)
+            aligner_path = path.join(aligned_audio_base, "mfa_aligner")
+            adjusted_queue_path = path.join(aligned_audio_base, "adjusted_corpus", channel_id, video_id, "queue")
+            adjusted_audio_path = path.join(aligned_audio_base, "adjusted_corpus", channel_id, video_id, "audio")
+            adjusted_tg_path = path.join(aligned_audio_base, "adjusted_corpus", channel_id, video_id, "textgrids")
 
-            for dir in [out_audpath, out_tgpath, prealignpath, postalignpath, alignerpath, adjusted_queuepath, adjusted_audpath, adjusted_tgpath]:
+            for dir in [out_audio_path, out_tg_path, pre_align_path, post_align_path, aligner_path, adjusted_queue_path, adjusted_audio_path, adjusted_tg_path]:
                 if not path.exists(dir):
                     makedirs(dir)
 
@@ -75,9 +68,9 @@ def main(args):
                     if not pd.isnull(row['transcription']):
 
                         name, ext = path.splitext(row['filename'])
-                        wav_filename = path.join(audpath, row['filename'])
+                        wav_fn = path.join(audio_path, row['filename'])
 
-                        sound = parselmouth.Sound(wav_filename)
+                        sound = parselmouth.Sound(wav_fn)
                         sound = call([sil1, sound, sil2], 'Concatenate')
                         sound_start = sound.get_start_time()
                         sound_end = sound.get_end_time()
@@ -89,16 +82,16 @@ def main(args):
                         call(textgrid, 'Set interval text', 1, 2, row['transcription'])
 
                         # Save to local dir
-                        sound.save(path.join(out_audpath, name+'.wav'), "WAV")
-                        textgrid.save(path.join(out_tgpath, name+'.TextGrid'))
+                        sound.save(path.join(out_audio_path, name+'.wav'), "WAV")
+                        textgrid.save(path.join(out_tg_path, name+'.TextGrid'))
 
                         # Save copy to temp MFA pre-alignment dir
-                        sound.save(path.join(prealignpath, name+'.wav'), "WAV")
-                        textgrid.save(path.join(prealignpath, name+'.TextGrid'))
+                        sound.save(path.join(pre_align_path, name+'.wav'), "WAV")
+                        textgrid.save(path.join(pre_align_path, name+'.TextGrid'))
 
     out_message = path.join(aligned_audio_base, "adjusted_corpus", "README.md")
-    with open(out_message, 'w') as m:
-        m.write('Channel folders for aligned audio files (found within the "queue" folder of each video sub-folder) go here.')    
+    with open(out_message, 'w') as file:
+        file.write('Channel folders for aligned audio files (found within the "queue" folder of each video sub-folder) go here.')
 
 if __name__ == '__main__':
 
@@ -107,7 +100,6 @@ if __name__ == '__main__':
     parser.set_defaults(func=None)
     parser.add_argument('--group', '-g', default=None, type=str, help='grouping folder')
     parser.add_argument('--channel', '-ch', default=None, type=str, help='channel folder')
-    parser.add_argument('--video', '-v', default=None, type=str, help='video number')
     parser.add_argument('--overwrite', '-o', action='store_true', default=False, help='overwrite files rather than appending')
 
     args = parser.parse_args()

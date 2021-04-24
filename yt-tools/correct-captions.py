ebf0db0b142d599c11fecabc69de81cb616837ca
Lauretta S.P. Cheng <53014890+lspcheng@users.noreply.github.com>
2021-04-24 12:47:29 -0400

    Add channel-specific processing

--- yt-tools/correct-captions.py
+++ yt-tools/correct-captions.py
@@ -13,6 +13,7 @@ try:
     import Tkinter as tk  # Python2
 except ImportError:
     import tkinter as tk  # Python3
+from tkinter.messagebox import showinfo
 from tkinter.filedialog import askopenfilename
 from functools import partial
 
@@ -25,21 +26,21 @@ def open_file_in_editor (file):
     else:
         print('This editor is not available. Please choose an available editor: textedit, atom')
 
-def open_video_and_subtitles (args, logfile, log, display, end_time, complete):
+def open_video_and_subtitles (args, log_fp, log, display, end_time, complete):
     if i == len(log):
         sys.exit('\nAll videos have been corrected!')
     else:
         row = log.iloc[i]
         timestamp = str(row['corrected'])
 
-        channel = sub(r"[\s\_\-\.\?\!,;:'\"\\\/]+", "", row['author'])
-        video_id = '{0}_{1}'.format(channel, row['position'])
+        channel_id = "{0}_{1}".format(row['name'], row['ID'])
+        video_id = '{0}_{1}'.format(channel_id, row['yt_id'])
 
-        print('\nOpening video {0}: {1} (starting at {2})'.format(i+1, video_id, timestamp))
+        print('\nOpening video {0}: {1} (starting at {2})'.format(i, video_id, timestamp))
 
         # Clear and configure displays
         complete.set(0)
-        display.config(text="Current Video: {0}".format(video_id))
+        display.config(text="Channel: {0}\tCurrent Video: {1}".format(row['name'], row['yt_id']))
         end_time.delete(0, 'end')
         if timestamp == '0':
             end_time.insert(0, '00:00:00')
@@ -50,28 +51,38 @@ def open_video_and_subtitles (args, logfile, log, display, end_time, complete):
         if args.group:
             raw_subtitles_base = path.join(raw_subtitles_base, args.group)
 
-        manual_dir = path.join(raw_subtitles_base, "manual", args.language, channel)
-        auto_dir = path.join(raw_subtitles_base, "auto", args.language, channel)
-        corrected_dir = path.join(raw_subtitles_base, "corrected", args.language, channel)
+        if args.language:
+            language = args.language
+        elif path.isdir(path.join(raw_subtitles_base, "manual")):
+            language_list = listdir(path.join(raw_subtitles_base, "manual"))
+            language = language_list[0]
+        elif path.isdir(path.join(raw_subtitles_base, "auto")):
+            language_list = listdir(path.join(raw_subtitles_base, "auto"))
+            language = language_list[0]
+
+        manual_dir = path.join(raw_subtitles_base, "manual", language, channel_id)
+        print(manual_dir)
+        auto_dir = path.join(raw_subtitles_base, "auto", language, channel_id)
+        corrected_dir = path.join(raw_subtitles_base, "corrected", language, channel_id)
         if not path.exists(corrected_dir):
             makedirs(corrected_dir)
 
         # Open .srt file in Text Editor
-        subfilename = '{0}.srt'.format(video_id)
+        sub_fn = '{0}.srt'.format(video_id)
 
         try:
-            subfile = path.join(manual_dir, subfilename)
-            if path.exists(subfile):
-                correctfile = path.join(corrected_dir, subfilename)
-                if not path.exists(correctfile):
-                    shutil.copyfile(subfile, correctfile)
-                open_file_in_editor(correctfile)
+            sub_fp = path.join(manual_dir, sub_fn)
+            if path.exists(sub_fp):
+                corrected_fp = path.join(corrected_dir, sub_fn)
+                if not path.exists(corrected_fp):
+                    shutil.copyfile(sub_fp, corrected_fp)
+                open_file_in_editor(corrected_fp)
             else:
-                subfile = path.join(auto_dir, subfilename)
-                correctfile = path.join(corrected_dir, subfilename)
-                if not path.exists(correctfile):
-                    shutil.copyfile(subfile, correctfile)
-                open_file_in_editor(correctfile)
+                sub_fp = path.join(auto_dir, sub_fn)
+                corrected_fp = path.join(corrected_dir, sub_fn)
+                if not path.exists(corrected_fp):
+                    shutil.copyfile(sub_fp, corrected_fp)
+                open_file_in_editor(corrected_fp)
         except FileNotFoundError:
             print('File does not exist.')
 
@@ -89,31 +100,36 @@ def open_video_and_subtitles (args, logfile, log, display, end_time, complete):
         except:
             print('Could not open URL in browser.')
 
-def save_progress (logfile, log, end_time, complete):
+def save_progress (log_fp, log, end_time, complete):
     global i
     if complete.get() == True:
         log.loc[i, 'corrected'] = 1
-        log.to_csv(logfile, index=False)
+        log.to_csv(log_fp, index=False)
         # display.config(text="Saved completion!"")
     elif match(r'\d+:\d+(:\d+)?', end_time.get()):
         log.loc[i, 'corrected'] = end_time.get()
-        log.to_csv(logfile, index=False)
+        log.to_csv(log_fp, index=False)
     else:
         print('Error: No time provided or completion not checked.')
 
-def save_and_quit(logfile, log, end_time, complete):
-    save_progress (logfile, log, end_time, complete)
+def save_and_quit(log_fp, log, end_time, complete):
+    save_progress (log_fp, log, end_time, complete)
     sys.exit('\nSafely saved progress!')
 
-def next_video (args, logfile, log, display, end_time, complete):
-    save_progress (logfile, log, end_time, complete)
+def next_video (args, log_fp, log, display, end_time, complete):
+    save_progress (log_fp, log, end_time, complete)
     global i
     i += 1
+    if args.group and args.channel:
+        while not log.iloc[i]['name'] == args.channel.split('_')[0]:
+            i += 1
+            if i >= len(log):
+                break
     if i >= len(log):
         sys.exit('\nAll videos have been corrected!')
     else:
         print('Next video: {0}'.format(i))
-    open_video_and_subtitles (args, logfile, log, display, end_time, complete)
+    open_video_and_subtitles (args, log_fp, log, display, end_time, complete)
 
 
 def main (args):
@@ -122,21 +138,34 @@ def main (args):
 
     # Open log file
     if args.group:
-        logfilename = '{0}_log.csv'.format(args.group)
-        logfile = path.join('corpus', 'logs', logfilename)
+        showinfo('Window', "Opening log file for group: {0}".format(args.group))
+        log_fn = '{0}_log.csv'.format(args.group)
+        log_fp = path.join('corpus', 'logs', log_fn)
+    elif args.channel:
+        showinfo('Window', "Opening log file for channel: {0}".format(args.channel))
+        log_fn = '{0}_log.csv'.format(args.channel)
+        log_fp = path.join('corpus', 'logs', log_fn)
     else:
-        logfile = askopenfilename()
+        showinfo('Window', "No group was specified. Select a channel log file from corpus > logs.")
+        log_fp = askopenfilename()
+        if log_fp == '':
+            sys.exit('No file selected. Exiting program now.')
 
-    log = pd.read_csv(logfile)
+    log = pd.read_csv(log_fp)
 
     # If column doesn't exist, initialize
     if not 'corrected' in log.columns:
         log = log.assign(corrected = 0)
 
     # Check rows in need of correction
-    i_list = [(i, row['author'], row['corrected']) for i, row in log.iterrows() if not str(row['corrected']) == '1']
+    i_list = [(i, row['name'], row['yt_id'], row['corrected']) for i, row in log.iterrows() if not str(row['corrected']) == '1']
+
+    if args.group and args.channel:
+        i_list = [item for item in i_list if item[1] == args.channel.split('_')[0]]
+
     print('\nNumber of videos remaining: {0}'.format(len(i_list)))
-    print(i_list)
+    for item in i_list:
+        print("{0}. {1}: {2} ({3})".format(item[0], item[1], item[2], item[3]))
 
     # If all files are completed, exit program
     if len(i_list) == 0:
@@ -151,7 +180,7 @@ def main (args):
     frame = tk.Frame(root)
     frame.grid(row=8, column=8, padx=10, pady=10)
 
-    display = tk.Label(frame, text="Current Video: {0}".format("None"))
+    display = tk.Label(frame, text="Channel: {0}\tCurrent Video: {1}".format("None", "None"))
     display.grid(row=0, column=1, columnspan=2)
 
     tk.Label(frame, text="Leaving off at time:").grid(row=3, column=1)
@@ -165,23 +194,22 @@ def main (args):
     complete = tk.IntVar()
     tk.Checkbutton(frame, text='Yes', variable=complete, onvalue=1, offvalue=0).grid(row=4, column=2)
 
-    tk.Button(frame, text="   Open   ", command=partial(open_video_and_subtitles, args, logfile, log, display, end_time, complete), bg="grey").grid(row=1, column=0)
-    tk.Button(frame, text=" Save Progress ", command=partial(save_progress, logfile, log, end_time, complete), bg="grey").grid(row=1, column=1)
-    tk.Button(frame, text= "  Save & Quit  ", command=partial(save_and_quit, logfile, log, end_time, complete), bg="grey").grid(row=1, column=2)
-    tk.Button(frame, text="   Next   ", command=partial(next_video, args, logfile, log, display, end_time, complete), bg="grey").grid(row=1, column=3)
+    tk.Button(frame, text="   Open   ", command=partial(open_video_and_subtitles, args, log_fp, log, display, end_time, complete), bg="grey").grid(row=1, column=0)
+    tk.Button(frame, text=" Save Progress ", command=partial(save_progress, log_fp, log, end_time, complete), bg="grey").grid(row=1, column=1)
+    tk.Button(frame, text= "  Save & Quit  ", command=partial(save_and_quit, log_fp, log, end_time, complete), bg="grey").grid(row=1, column=2)
+    tk.Button(frame, text="   Next   ", command=partial(next_video, args, log_fp, log, display, end_time, complete), bg="grey").grid(row=1, column=3)
 
 
     root.mainloop()
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='Create MFA-compatible textgrids and move to MFA alignment folder.')
+    parser = argparse.ArgumentParser(description='Open text file and YouTube video to aid in correcting captions.')
 
     parser.set_defaults(func=None)
     parser.add_argument('--group', '-g', default=None, type=str, help='grouping folder')
     parser.add_argument('--language', '-l', default=None, type=str, help='language code')
     parser.add_argument('--channel', '-ch', default=None, type=str, help='channel folder')
-    parser.add_argument('--video', '-v', default=None, type=str, help='video number')
     parser.add_argument('--editor', '-e', default='textedit', type=str, help='preferred text editor')
 
     args = parser.parse_args()

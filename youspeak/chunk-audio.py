451a26f5df83d0e4c8ff61da01369606df1b3c59
Lauretta S.P. Cheng <53014890+lspcheng@users.noreply.github.com>
2021-04-29 21:52:59 -0400

    Add textgrid_only mode and corrected textgrid input route (untested)

--- youspeak/chunk-audio.py
+++ youspeak/chunk-audio.py
@@ -102,7 +102,7 @@ def save_chunks(chunk_sound, out_path, video_id):
     return {'filename': chunk_fn, 'video_id': video_id, 'start_time': chunk_start_ms, 'end_time': chunk_end_ms, 'duration': chunk_duration}
 
 
-def process_soundfile(fn, audio_path, chunk_path, overwrite=False):
+def process_soundfile(fn, audio_path, chunk_path, overwrite=False, textgrid_only=False):
 
     video_id, ext = path.splitext(fn)
 
@@ -118,26 +118,43 @@ def process_soundfile(fn, audio_path, chunk_path, overwrite=False):
             if existing_files:
                 return 1
 
-        # Create log file
-        log_file = path.join(log_path, video_id+'_chunking_log.csv')
-        if not path.exists(log_path):
-            makedirs(log_path)
+        if not textgrid_only:
+            # Create log file
+            log_file = path.join(log_path, video_id+'_chunking_log.csv')
+            if not path.exists(log_path):
+                makedirs(log_path)
 
-        output_df = pd.DataFrame(columns=['filename','video_id',
-                                          'start_time','end_time', 'duration'])
-        output_df.to_csv(log_file, index=False)
+            output_df = pd.DataFrame(columns=['filename','video_id',
+                                              'start_time','end_time', 'duration'])
+            output_df.to_csv(log_file, index=False)
 
+            # Create output directory
+            if not path.exists(sound_path):
+                makedirs(sound_path)
 
-        # Create output directory
-        if not path.exists(sound_path):
-            makedirs(sound_path)
+        if not path.exists(tg_path):
+            makedirs(tg_path)
 
         # Start audio processing
         print('\nCURRENT FILE: {0}'.format(fn))
 
+        tg_fn = path.join(tg_path, video_id+'.TextGrid')
+        tg_fn_cor = path.join(tg_path, video_id+'_corrected.TextGrid')
+
         wav_fn = path.join(audio_path, fn)
         sound = parselmouth.Sound(wav_fn).convert_to_mono()
 
+        if path.exists(tg_fn_cor):
+            textgrid = parselmouth.read(tg_fn_cor)
+            extracted_sounds = call([sound, textgrid],
+                                    'Extract intervals where',
+                                    1, True, 'is equal to', 'speech')
+            for subsound in extracted_sounds:
+                log_entry = save_chunks(subsound, sound_path, video_id)
+                output_df = output_df.append(log_entry, ignore_index=True)
+            output_df = output_df.sort_values(by=["start_time"])
+            output_df.to_csv(log_file, mode='a', index=False, header=False)
+            return 2
 
         print('First pass chunking in progress...')
         # Use a more conservative 0.5 sec silence to get larger chunks
@@ -149,12 +166,6 @@ def process_soundfile(fn, audio_path, chunk_path, overwrite=False):
         while n_ints <= 1:
             quantile += 0.025
             (base_textgrid, extracted_sounds_1, n_ints) = chunk_sound(sound, sil_duration, quantile)
-
-        # Save first-pass TextGrid
-        if not path.exists(tg_path):
-            makedirs(tg_path)
-        tg_fn = path.join(tg_path, video_id+'_first.TextGrid')
-        base_textgrid.save(tg_fn)
 
         print('Second pass chunking in progress...')
         counter = -1
@@ -178,20 +189,23 @@ def process_soundfile(fn, audio_path, chunk_path, overwrite=False):
             for subsound in extracted_sounds_1:
                 duration = subsound.get_total_duration()
                 if duration <= 10:
-                    log_entry = save_chunks(subsound, sound_path, video_id)
-                    output_df = output_df.append(log_entry, ignore_index=True)
+                    if not textgrid_only:
+                        log_entry = save_chunks(subsound, sound_path, video_id)
+                        output_df = output_df.append(log_entry, ignore_index=True)
 
                     # Add boundary to base_textgrid
+                    subsound_start = subsound.get_start_time()
+                    subsound_end = subsound.get_end_time()
                     try:
-                        call(base_textgrid, 'Insert boundary', 1, (log_entry['start_time']/1000) )
+                        call(base_textgrid, 'Insert boundary', 1, subsound_start)
                     except:
-                        print('\nNo boundary inserted at time {0}.'.format(log_entry['start_time']/1000))
+                        print('\nNo boundary inserted at time {0}.'.format(subsound_start)
                     try:
-                        call(base_textgrid, 'Insert boundary', 1, (log_entry['end_time']/1000) )
+                        call(base_textgrid, 'Insert boundary', 1, subsound_end)
                     except:
-                        print('\nNo boundary inserted at time {0}.'.format(log_entry['start_time']/1000))
+                        print('\nNo boundary inserted at time {0}.'.format(subsound_end)
 
-                    interval_num = call(base_textgrid, 'Get interval at time', 1, log_entry['start_time']/1000)
+                    interval_num = call(base_textgrid, 'Get interval at time', 1, subsound_start)
                     call(base_textgrid, 'Set interval text', 1, interval_num, 'speech')
 
                     extracted_sounds_1.remove(subsound)
@@ -208,12 +222,12 @@ def process_soundfile(fn, audio_path, chunk_path, overwrite=False):
                         else:
                             sub_quantile += 0.025
 
-        output_df = output_df.sort_values(by=["start_time"])
-        output_df.to_csv(log_file, mode='a', index=False, header=False)
+        if not textgrid_only:
+            output_df = output_df.sort_values(by=["start_time"])
+            output_df.to_csv(log_file, mode='a', index=False, header=False)
 
         # Save second-pass TextGrid
         call(base_textgrid, 'Replace interval texts', 1, 1, 0, '', 'silence', 'Literals')
-        tg_fn = path.join(tg_path, video_id+'_second.TextGrid')
         base_textgrid.save(tg_fn)
 
 def main(args):
@@ -229,9 +243,9 @@ def main(args):
         if path.isdir(path.join(audio_path, dir_element)):
             channel_audio_path = path.join(audio_path, dir_element)
             for fn in listdir(channel_audio_path):
-                process_soundfile(fn, channel_audio_path, chunk_path, args.overwrite)
+                process_soundfile(fn, channel_audio_path, chunk_path, args.overwrite, args.textgrid_only)
         else:
-            process_soundfile(dir_element, audio_path, chunk_path, args.overwrite)
+            process_soundfile(dir_element, audio_path, chunk_path, args.overwrite, args.textgrid_only)
 
     out_message = path.join(chunk_path, "audio", "chunking", "README.md")
     with open(out_message, 'w') as file:
@@ -243,6 +257,7 @@ if __name__ == '__main__':
 
     parser.set_defaults(func=None)
     parser.add_argument('--group', '-g', default=None, type=str, help='name to group files under (create and /or assume files are located in a subfolder: raw_subtitles/$group)')
+    parser.add_argument('--textgrid_only', '-tg', action='store_true', default=False, help='only output textgrid with detected speech; no chunked sound files will be saved')
     parser.add_argument('--overwrite', '-o', action='store_true', default=False, help='overwrite files rather than appending')
 
     args = parser.parse_args()

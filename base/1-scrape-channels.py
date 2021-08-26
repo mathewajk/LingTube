858b54d667a0363a89bb2bb17ba92c3fd3e1ee52
Mathew A. K <a.kramer103@gmail.com>
2021-08-26 15:49:24 -0400

    convert ChannelScraper to handle channel+video input

--- base/1-scrape-channels.py
+++ base/1-scrape-channels.py
@@ -11,29 +11,22 @@ def run(args):
     scraper = None
 
     source        = args.source
+    from_video    = args.v
     browser       = args.browser
     group         = args.group
     pause_time    = args.wait
     cutoff        = args.limit
     ignore_videos = args.novideos
-    screen        = args.screen
     overwrite     = args.overwrite
+    screen        = args.screen
 
-    if args.c:
-        if 'http' in source:
-            scraper = Base.ChannelHandler(source, browser, pause_time, cutoff, group, ignore_videos, screen)
-        else:
-            scraper = Base.MultiChannelHandler(source, browser, pause_time, cutoff, group, ignore_videos, screen)
-    elif args.v:
-        if 'http' in source:
-            scraper = Base.VideoHandler(source, browser, pause_time, cutoff, group, ignore_videos, overwrite, screen)
-        else:
-            scraper = Base.MultiVideoHandler(source, browser, pause_time, cutoff, group, ignore_videos, overwrite, screen)
+    if 'http' in source:
+        scraper = Base.ChannelHandler(source, from_video, browser, pause_time, cutoff, group, ignore_videos, overwrite, screen)
     else:
-        print('ERROR: Please specify whether the input contains channel(s) (-c) or video(s) (-v)')
-        exit(1)
+        scraper = Base.MultiChannelHandler(source, from_video, browser, pause_time, cutoff, group, ignore_videos, overwrite, screen)
 
     scraper.process()
+
 
 if __name__ == '__main__':
 
@@ -41,14 +34,13 @@ if __name__ == '__main__':
 
     parser.add_argument('source', type=str, help='URL or file path to list of URLs to scrape')
 
-    parser.add_argument('-c', '-channel',    action='store_true', default=False, help='Source is a channel or list of channels')
-    parser.add_argument('-v', '-video',      action='store_true', default=False, help='Source is a video or list of videos')
+    parser.add_argument('-v', '-video',   action='store_true', default=False, help='Source is a video or list of videos')
     parser.add_argument('-g', '--group',     default=None, type=str, help='grouping for the output files (will create a subfolder, e.g., screened_urls/$group)')
     parser.add_argument('-b', '--browser',   default="Firefox", type=str, help='browser to use for scraping ("Firefox" or "Chrome")')
-    parser.add_argument('-n', '--novideos',  action='store_true', default=False, help='only scrape about page(s); ignore video URLs')
+    parser.add_argument('-n', '--novideos',  action='store_true', default=False, help='only scrape about page(s); do not scrape video URLs')
     parser.add_argument('-l', '--limit',     type=int, default=-1, help='maximum number of times to scroll the page when scraping videos')
     parser.add_argument('-w', '--wait',      type=int, default=1, help='how long to pause between scrolls; increase for slower connections')
-    parser.add_argument('-s', '--screen',    action='store_true', default=False, help='download files for screening purposes')
+    parser.add_argument('-s', '--screen',    action='store_true', default=False, help='videos require screening for adequacy')
     parser.add_argument('-o', '--overwrite', action='store_true', default=False, help='overwrite files rather than appending (applies to video lists only)')
 
     args = parser.parse_args()

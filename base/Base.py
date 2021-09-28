37cca14f9f7a06edb93ca2108796d286229228d4
Mathew A. K <a.kramer103@gmail.com>
2021-09-28 11:48:16 -0400

    Fix indentation

--- base/Base.py
+++ base/Base.py
@@ -431,54 +431,54 @@ class VideoScraper:
 
 
     def xml_caption_to_srt(self, xml_captions: str) -> str:
-      """Convert xml caption tracks to "SubRip Subtitle (srt)".
+        """Convert xml caption tracks to "SubRip Subtitle (srt)".
 
-      :param str xml_captions:
-          XML formatted caption tracks.
-      """
-      segments = []
-      root = ElementTree.fromstring(xml_captions)[1]
-      i=0
-      for child in list(root):
-          if child.tag == 'p':
-              caption = ''
-              if len(list(child))==0:
-                  continue
-              for s in list(child):
-                  if s.tag == 's':
-                      caption += ' ' + s.text
-              caption = unescape(caption.replace("\n", " ").replace("  ", " "),)
-              try:
-                  duration = float(child.attrib["d"])/1000.0
-              except KeyError:
-                  duration = 0.0
-              start = float(child.attrib["t"])/1000.0
-              end = start + duration
-              sequence_number = i + 1  # convert from 0-indexed to 1.
-              line = "{seq}\n{start} --> {end}\n{text}\n".format(
-                  seq=sequence_number,
-                  start=self.float_to_srt_time_format(start),
-                  end=self.float_to_srt_time_format(end),
-                  text=caption,
-              )
-              segments.append(line)
-              i += 1
+        :param str xml_captions:
+        XML formatted caption tracks.
+        """
+        segments = []
+        root = ElementTree.fromstring(xml_captions)[1]
+        i=0
+        for child in list(root):
+            if child.tag == 'p':
+                caption = ''
+                if len(list(child))==0:
+                    continue
+                for s in list(child):
+                    if s.tag == 's':
+                        caption += ' ' + s.text
+                caption = unescape(caption.replace("\n", " ").replace("  ", " "),)
+                try:
+                    duration = float(child.attrib["d"])/1000.0
+                except KeyError:
+                    duration = 0.0
+                start = float(child.attrib["t"])/1000.0
+                end = start + duration
+                sequence_number = i + 1  # convert from 0-indexed to 1.
+                line = "{seq}\n{start} --> {end}\n{text}\n".format(
+                    seq=sequence_number,
+                    start=self.float_to_srt_time_format(start),
+                    end=self.float_to_srt_time_format(end),
+                    text=caption,
+                )
+            segments.append(line)
+            i += 1
       return "\n".join(segments).strip()
 
 
     def float_to_srt_time_format(self, d: float) -> str:
-            """Convert decimal durations into proper srt format.
+        """Convert decimal durations into proper srt format.
 
-            :rtype: str
-            :returns:
-                SubRip Subtitle (str) formatted time duration.
+        :rtype: str
+        :returns:
+            SubRip Subtitle (str) formatted time duration.
 
-            float_to_srt_time_format(3.89) -> '00:00:03,890'
-            """
-            fraction, whole = math.modf(d)
-            time_fmt = time.strftime("%H:%M:%S,", time.gmtime(whole))
-            ms = f"{fraction:.3f}".replace("0.", "")
-            return time_fmt + ms
+        float_to_srt_time_format(3.89) -> '00:00:03,890'
+        """
+        fraction, whole = math.modf(d)
+        time_fmt = time.strftime("%H:%M:%S,", time.gmtime(whole))
+        ms = f"{fraction:.3f}".replace("0.", "")
+        return time_fmt + ms
 
     def write_audio(self, audio):
         """Write audio Stream object to a file. If an output folder is not specified, audio will be placed in a folder corresponding to the name of the video's author (i.e. channel).
